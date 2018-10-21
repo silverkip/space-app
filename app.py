b57b325fd41cc89e1488696e665771e228c2d859
Lyova <iamsuperman145@gmail.com>
2018-10-21 01:19:19 -0400

    chenged config

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def divTemplate(idx, row):
 app.layout = html.Div(
     className='main',
     children=[
-        html.A(href="https://clever-boyd-6ef0a3.netlify.com/", id="ref", children="Info"),
+        html.A(href="https://clever-boyd-6ef0a3.netlify.com/", className="ref", children="Info"),
         html.H1(id="name", children='LAUNCH.IO'),
         html.H1(
             id='Timer',
@@ -109,7 +109,7 @@ app.layout = html.Div(
                                 lon=-73
                             ),
                             pitch=0,
-                            zoom=1
+                            zoom=2
                         )
                     )
                 ),
